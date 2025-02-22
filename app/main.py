873d75c24d1b466956b97aa77885f6a2d624af2b
ReetamBG <r33t4m@gmail.com>
2025-02-22 16:40:15 +0530

    Added more debugging

--- app/main.py
+++ app/main.py
@@ -4,12 +4,14 @@ from src.pipelines.predict_pipeline import Predictor
 
 data = pd.read_csv("artifacts/raw_data.csv")
 
-print("📁 File Loaded Successfully!")
-print("📁 Data:", data)
-print("📝 Columns in Data:", list(data.columns))
-print("🔍 First 5 Rows:\n", data.head())
+# print("📁 File Loaded Successfully!")
+# print("📁 Data:", data)
+# print("📝 Columns in Data:", list(data.columns))
+# print("🔍 First 5 Rows:\n", data.head())
 
 st.title("Used Car Price Predictor")
+st.dataframe(data)
+st.subheader(data.columns)
 
 form = st.form("car_details_form")
 
